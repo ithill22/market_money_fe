fd598da5e4b454b99fde6637bde9f445651b07db
Isaac Thill <ithill22@turing.edu>
2023-05-24 18:02:51 -0600

    setup 2

--- app/facades/markets_facade.rb
+++ app/facades/markets_facade.rb
@@ -1,4 +1,4 @@
-class MmFacade
+class MarketsFacade
   def get_markets
     service.get_markets.map do |market_data|
       Market.new(market_data)
@@ -8,17 +8,6 @@ class MmFacade
   def get_market(market_id)
     market_data = service.get_market(market_id)
     Market.new(market_data)
-  end
-
-  def get_market_vendors(market_id)
-    service.get_market_vendors(market_id).map do |vendor_data|
-      Vendor.new(vendor_data)
-    end
-  end
-
-  def get_vendor(vendor_id)
-    vendor_data = service.get_vendor(vendor_id)
-    Vendor.new(vendor_data)
   end
 
   private
---
+++ app/facades/vendors_facade.rb
@@ -0,0 +1,18 @@
+class VendorsFacade
+  def get_market_vendors(market_id)
+    service.get_market_vendors(market_id).map do |vendor_data|
+      Vendor.new(vendor_data)
+    end
+  end
+
+  def get_vendor(vendor_id)
+    vendor_data = service.get_vendor(vendor_id)
+    Vendor.new(vendor_data)
+  end
+
+  private
+  def service
+    _service ||= MmService.new
+  end
+end
+end
